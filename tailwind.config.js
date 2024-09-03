925433c96460a6b706594184ac931529262217d5
Raees Mughal <raees.mughal.official@gmail.com>
2024-09-03 22:29:49 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily : {
+        custom : ["Sevillana", "cursive"]
+      }
+    },
+  },
+  plugins: [],
+}
+
