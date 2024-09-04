d979e21a880fd3cb6468100e62ad3657459431da
Raees Mughal <raees.mughal.official@gmail.com>
2024-09-05 00:31:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+
+
+
+let hamburgerMenu = document.getElementById('hamburger-menu');
+let closeMenu = document.getElementById('close-menu');
+let menuMobile = document.getElementById('menu-mobile');
+
+
+
+
+hamburgerMenu.addEventListener('click',()=>{
+    menuMobile.classList.remove('hidden');
+})
+closeMenu.addEventListener('click',()=>{
+    menuMobile.classList.add('hidden');
+})
+
+
